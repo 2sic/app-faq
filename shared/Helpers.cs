f7bec90b4b17360521a6c9e9a8486e746e44956d
ijungleboy <daniel.mettler@2sic.com>
2023-05-30 22:33:29 +0200

    going typed on FAQ

--- shared/Helpers.cs
+++ shared/Helpers.cs
@@ -1,17 +1,27 @@
-using ToSic.Razor.Blade;
 using System.Linq;
 using System;
 using System.Collections.Generic;
+using ToSic.Razor.Blade;
+using ToSic.Sxc.Data;
 
 public class Helpers: Custom.Hybrid.Code12
 {
-  /// <summary>
-  /// Create a hover-help-label for admins to better manage the questions
-  /// </summary>
+  // TODO: @2dg - remove this after refactoring BS3
   public object AdminHelperLabel(dynamic q) {
     var itemCategories = AsList(q.Categories as object).Select(cat => cat.Name);
     var label = q.InternalTitle + ", "
       + "Prio: " + q.Priority + ", "
+      + "Categories: " + String.Join(",", itemCategories);
+    return Edit.Attribute("title", label);
+  }
+
+  /// <summary>
+  /// Create a hover-help-label for admins to better manage the questions
+  /// </summary>
+  public IHtmlTag AdminHelperLabel(ITypedItem q) {
+    var itemCategories = q.Children("Categories").Select(cat => cat.String("Name"));
+    var label = q.String("InternalTitle") + ", "
+      + "Prio: " + q.Int("Priority") + ", "
       + "Categories: " + String.Join(",", itemCategories);
     return Edit.Attribute("title", label);
   }
