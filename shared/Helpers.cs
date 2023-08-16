27d0f8f2bb6aa075e9660e28d36f455cf120cf91
Roman Opalko <roman.opalko@2sic.com>
2023-08-16 14:01:58 +0200

    renamed Custom.Hybrid... to ...Typed

--- shared/Helpers.cs
+++ shared/Helpers.cs
@@ -3,7 +3,7 @@ using System;
 using ToSic.Razor.Blade;
 using ToSic.Sxc.Data;
 
-public class Helpers: Custom.Hybrid.CodePro
+public class Helpers: Custom.Hybrid.CodeTyped
 {
   /// <summary>
   /// Create a hover-help-label for admins to better manage the questions
