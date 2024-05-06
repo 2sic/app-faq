c916e30e5bce43d9fc357632fc154b26996ad095
iJungleboy <daniel.mettler@2sic.com>
2024-05-06 17:12:48 +0200

    minor bugfix

--- AppCode/Razor/AppRazor.cs
+++ AppCode/Razor/AppRazor.cs
@@ -1,5 +1,6 @@
 using AppCode.Data;
 using System.Linq;
+using ToSic.Razor.Markup;
 
 namespace AppCode.Razor
 {
@@ -9,7 +10,7 @@ namespace AppCode.Razor
     /// Create a hover-help-label for admins to better manage the questions
     /// In this case, return can by a Object as it is only displayed and is not processed further but the Type are kept for better readability (IHtmlString)
     /// </summary>
-    public System.Web.IHtmlString AdminHelperLabel(Question question)
+    public IRawHtmlString AdminHelperLabel(Question question)
     {
       var itemCategories = question.Categories.Select(cat => cat.Name);
       var label = question.InternalTitle + ", "
